b8d1cf455df470d43c3f283623ef0dba7e615313
Katrindenek <zapkokaty@gmail.com>
2024-03-28 17:43:14 +0000

    another commit

--- models/business_vault/as_of_date.sql
+++ models/business_vault/as_of_date.sql
@@ -1,8 +1,8 @@
 {{ config(materialized='table') }}
 
 {%- set datepart = "day" -%}
-{%- set start_date = "TO_DATE('2024/03/10', 'yyyy/mm/dd')" -%}
-{%- set end_date = "TO_DATE('2024/03/17', 'yyyy/mm/dd')" -%}
+{%- set start_date = "TO_DATE('2024/03/01', 'yyyy/mm/dd')" -%}
+{%- set end_date = "TO_DATE('2024/03/31', 'yyyy/mm/dd')" -%}
 
 WITH as_of_date AS (
     {{ dbt_utils.date_spine(datepart=datepart, 
@@ -11,3 +11,4 @@ WITH as_of_date AS (
 )
 
 SELECT DATE_{{datepart}} as AS_OF_DATE FROM as_of_date
+
--- models/mart/d_pit_customer_details.sql
+++ models/mart/d_pit_customer_details.sql
@@ -1,5 +1,25 @@
+WITH sat_with_effective_to AS (
+    SELECT 
+        scd.CUSTOMER_PK,
+        scd.first_name,
+        scd.last_name,
+        scd.email,
+        scd.EFFECTIVE_FROM,
+        LEAD(scd.EFFECTIVE_FROM, 1, date '9999-12-31') OVER(PARTITION BY scd.CUSTOMER_PK ORDER BY scd.EFFECTIVE_FROM) as EFFECTIVE_TO
+    FROM 
+        {{ref('sat_customer_details')}} scd
+)
 SELECT 
-    *
+    vpcd.as_of_date,
+    swet.CUSTOMER_PK,
+    swet.first_name,
+    swet.last_name,
+    swet.email,
+    swet.EFFECTIVE_FROM,
+    swet.EFFECTIVE_TO
 FROM 
-    {{ref('v_pit_customer_details')}} vpcd 
-    LEFT JOIN {{ref('sat_customer_details')}} scd ON vpcd.sat_customer_details_pk = scd.
+    {{ref('pit_customer_details')}} vpcd 
+    LEFT JOIN sat_with_effective_to swet ON vpcd.sat_customer_details_pk = swet.CUSTOMER_PK
+ORDER BY 
+    swet.CUSTOMER_PK,
+    vpcd.as_of_date
